26fbad7b74e7f488032bb66da8153f1c466effb5
yangjh <yangjh@yeah.net>
2019-05-30 17:12:17 +0800

    增加github仓库

--- _book/gitbook.sh
+++ _book/gitbook.sh
@@ -2,5 +2,7 @@ gitbook build
 git add .
 git commit -m $1
 git push -u origin master
+git push -u hub master
 open http://yangjh.gitee.io/tutor
 open https://gitee.com/yangjh
+open https://github.com/yangjh2019
